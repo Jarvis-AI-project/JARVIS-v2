1974090595ff26e154cf73d42d3f6a68322e6f9c
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-13 00:32:24 +0530

    .

--- wake-word/main.py
+++ wake-word/main.py
@@ -1,18 +1,48 @@
+import struct
+import pyaudio
 import pvporcupine
+import playsound
+import multiprocessing as mp
 
+porcupine=None
+paud=None
+audio_stream=None
 
-porcupine = pvporcupine.create(
-  access_key='MMvgSaJ5k6iELWtuyxz6R0nnz7exBVT56piX6Qq9cQViCVEK2OsbNQ==',
-  keywords=['JARVIS']
-)
+# C:\Users\swast\AppData\Local\Programs\Python\Python310\Lib\site-packages\pvporcupine
 
-def get_next_audio_frame():
-  pass
+def tune(music_file):
+    # play sound
+    playsound.playsound(music_file)    # pip install playsound==1.2.2
 
-while True:
-  audio_frame = get_next_audio_frame()
-  keyword_index = porcupine.process(audio_frame)
-  if keyword_index == 0:
-      # detected `porcupine`
-  elif keyword_index == 1:
-      # detected `bumblebee`
+def wake_word_detection(model, music_file = 'chime.wav'):
+    
+    tune_process = mp.Process(target=tune, args=(music_file,))
+
+    try:
+        porcupine=pvporcupine.create(
+            access_key='+m4ClCWe3QUlLBiYi9bIgjdboyQWIqDdnCkN3gUAnCDuJHF2L9ez8g==',
+            keyword_paths=model
+            ) 
+        paud=pyaudio.PyAudio()
+        audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
+        while True:
+            keyword=audio_stream.read(porcupine.frame_length)
+            keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
+            keyword_index=porcupine.process(keyword)
+            if keyword_index>=0:
+                print("hotword detected")
+                tune_process.start()
+                break
+                
+    finally:
+        if porcupine is not None:
+            porcupine.delete()
+        if audio_stream is not None:
+            audio_stream.close()
+        if paud is not None:
+            paud.terminate()
+
+if __name__ == '__main__':
+    
+    while True:
+        wake_word_detection(model=['jarvis_windows.ppn'])
