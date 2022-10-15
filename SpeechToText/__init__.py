fe2c3fcb7bcc311964f806aa0cc2585b092a2d69
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-15 22:04:26 +0530

    Update __init__.py

--- SpeechToText/__init__.py
+++ SpeechToText/__init__.py
@@ -5,16 +5,19 @@ def speech_to_text():
     try:
         # obtain audio from the microphone
         r = sr.Recognizer()
-        with sr.Microphone() as source:
+        with sr.Microphone() as audio_stream:
             print("Engine Listening...")
-            audio = r.listen(source, timeout=None, phrase_time_limit=None, snowboy_configuration=None)
+            audio = r.listen(source=audio_stream,
+                             timeout=None,
+                             phrase_time_limit=None,
+                             snowboy_configuration=None)
             print('Processing...')
-        # recognize speech using Google Speech Recognition
 
+        # recognize speech using Google Speech Recognition
         output = r.recognize_google(audio, language='en-US', show_all=True)
 
         if output is not None:
-            return {'text': output,
+            return {'text': output['alternative'][0]['transcript'],
                     'confidence': output['alternative'][0]['confidence']}
         elif output is None:
             return {'transcription': output, 'confidence': 0.0}
@@ -37,3 +40,14 @@ def speech_to_text():
 if __name__ == "__main__":
     while True:
         speech_to_text()
+
+
+# output format
+# {
+#         'alternative': [{'transcript': 'what is the time', 'confidence': 0.92995489},
+#                         {'transcript': 'what is the time now'},
+#                         {'transcript': 'what is the time Ruk Jana'},
+#                         {'transcript': 'what is the time Ludhiana'},
+#                         {'transcript': 'what is the time there'}],
+#         'final': True
+# }
