73fbe50d8e078a56a318798e1be094db4b30246e
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-15 20:55:38 +0530

    Update __init__.py

--- SpeechToText/__init__.py
+++ SpeechToText/__init__.py
@@ -1,28 +1,35 @@
 import speech_recognition as sr
 
-def speech_to_text():
-    # obtain audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Engine Listening...")
-        audio = r.listen(source, timeout=6)
 
-    # recognize speech using Google Speech Recognition
+def speech_to_text():
+
     try:
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Engine Listening...")
+            audio = r.listen(source, timeout=6)
+
+        # recognize speech using Google Speech Recognition
         if r.recognize_google(audio, show_all=True) is not None:
-            return {'text': r.recognize_google(audio), 
+            return {'text': r.recognize_google(audio),
                     'confidence': r.recognize_google(audio, show_all=True)['alternative'][0]['confidence']}
         else:
-            return {'text': r.recognize_google(audio), 'confidence': 0.0}
+            return {'transcription': r.recognize_google(audio), 'confidence': 0.0}
 
-    except sr.UnknownValueError as e:
+    except sr.UnknownValueError() as e:
         print("\nCould not Understand Audio : {0}".format(e))
         return {'type': 'Error', 'data': 'Understanding Audio Error'}
 
     except sr.RequestError as e:
-        print("\nCould not request results from Speech Recognition service : {0}".format(e))
+        print(
+            "\nCould not request results from Speech Recognition service : {0}".format(e))
         return {'type': 'Error', 'data': 'Connection Error'}
-    
+
+    except sr.WaitTimeoutError() as e:
+        return {'type': 'Error', 'data': 'Listning Timed Out While Waiting For Phrase to Start'}
+
+
 if __name__ == "__main__":
     while True:
         speech_to_text()
