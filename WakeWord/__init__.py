728ae3edb858694aa8b94c4210d2e040f05d380e
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-16 02:12:32 +0530

    .

--- WakeWord/__init__.py
+++ WakeWord/__init__.py
@@ -10,7 +10,7 @@ paud = None
 audio_stream = None
 
 access_keys = [
-    '+m4ClCWe3QUlLBiYi9bIgjdboyQWIqDdnCkN3gUAnCDuJHF2L9ez8g==',
+    'otyT/UGS6tKCiLj0cktSjxshuMSNt6+fYVmuwfscF1cZYiysbWdRVA==',
     'MMvgSaJ5k6iELWtuyxz6R0nnz7exBVT56piX6Qq9cQViCVEK2OsbNQ==',
     'pXwEvAXJABqQf8hMoHFFGcsgkabxMPQMttKsdeyXRxEAL6r0VTbf9w==']
 
@@ -40,6 +40,8 @@ def wake_word_detection(model):
                 except:
                     print('could not connect to hotword detection server number 3 \n')
                     print('hotword detection failed \n')
+                    return False
+
         print('hotword detection started')
         paud = pyaudio.PyAudio()
         audio_stream = paud.open(rate=porcupine.sample_rate,
@@ -48,13 +50,17 @@ def wake_word_detection(model):
                                  input=True,
                                  frames_per_buffer=porcupine.frame_length)
         while True:
-            keyword = audio_stream.read(porcupine.frame_length)
-            keyword = struct.unpack_from("h"*porcupine.frame_length, keyword)
-            keyword_index = porcupine.process(keyword)
-            if keyword_index >= 0:
-                print("hotword detected", keyword_index)
-                return True
+            try:
+                keyword = audio_stream.read(porcupine.frame_length)
+                keyword = struct.unpack_from("h"*porcupine.frame_length, keyword)
+                keyword_index = porcupine.process(keyword)
+                if keyword_index >= 0:
+                    print("hotword detected", keyword_index)
+                    return True
 
+            except KeyboardInterrupt:
+                print('You pressed ctrl C')
+                return 'KeyboardInterrupt'
     except:
         print('ERROR in hotword detection')
         return False
--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import WakeWord
 import SpeechToText.azure as STT
 import winsound
 while True:
+    wake = WakeWord.wake_word_detection(
+        model=['WakeWord\jarvis_windows.ppn', 'WakeWord\hey-jarvis_windows.ppn', 'WakeWord\hi-jarvis_en_windows.ppn'])
     try:
-        if WakeWord.wake_word_detection(model=['WakeWord\jarvis_windows.ppn', 'WakeWord\hey-jarvis_windows.ppn', 'WakeWord\hi-jarvis_en_windows.ppn']) == True:
+        if wake == True:
             winsound.Beep(2500, 100)
             output = STT.speech_to_text()
             print(output)
-        else:
+        elif wake == False:
             WakeWord.reset()
+        
+        elif wake == 'KeyboardInterrupt':
+            break
 
     except Exception as e:
         print('Unknown Error')
