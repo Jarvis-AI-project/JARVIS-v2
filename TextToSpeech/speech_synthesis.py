8c337d4083a28ae9f109396c6fdf00962e6fac07
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-16 17:10:32 +0530

    .

--- TextToSpeech/speech_synthesis.py
+++ TextToSpeech/speech_synthesis.py
@@ -2,14 +2,16 @@ import os
 import azure.cognitiveservices.speech as speechsdk
 
 # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+speech_config = speechsdk.SpeechConfig(
+    subscription='95aa2e3713c5421683334dd5decb6fff', region='westus')
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 
 # The language of the voice that speaks.
 speech_config.speech_synthesis_voice_name = 'en-US-JennyNeural'
 
 speech_synthesizer = speechsdk.SpeechSynthesizer(
-    speech_config=speech_config, audio_config=audio_config)
+    speech_config=speech_config, 
+    audio_config=audio_config)
 
 # Get text from the console and synthesize to the default speaker.
 print("Enter some text that you want to speak >")
