b40df012b0501dc8a49a8078b5783d69e9d5b52d
Dhruv-Singh-Poniya <113854082+Dhruv-Singh-Poniya@users.noreply.github.com>
2022-10-17 17:50:04 +0530

    Update temp.py

--- features/todoist/temp.py
+++ features/todoist/temp.py
@@ -1,3 +1,7 @@
-d={}
-d[1]=2
-print (d)
+from todoist_api_python.api import TodoistAPI
+api_token = 'ac6ec72a38a0590df35f986f6147d6f9b3e92622'
+api = TodoistAPI(api_token)
+
+for task in api.get_tasks():
+    print(task)
+     
