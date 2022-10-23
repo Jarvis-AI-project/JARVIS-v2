ec7901c8e8ced36b42bc848e25ef078e5187cd7b
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-23 16:22:18 +0530

    .

--- TextToSpeech/temp3.py
+++
@@ -1,5 +0,0 @@
-from voicemaker import Voicemaker
-
-vm = Voicemaker()
-vm.set_token('9729c060-4d7d-11ed-be8d-312d29edc20b')
-vm.generate_audio_to_file(out_path='test.mp3', text="I met a traveller from an antique land Who said: Two vast and trunkless legs of stone Stand in the desert.")
