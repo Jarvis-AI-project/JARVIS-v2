93c43782679a1757660483ea33932fdbde9a1f52
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-24 14:25:53 +0530

    updated files

---
+++ functions/WakeWord/__main__.py
@@ -0,0 +1,11 @@
+import __init__ as WakeWord
+import winsound
+while True:
+    try:
+        if WakeWord.wake_word_detection(model=['WakeWord\jarvis_windows.ppn', 'WakeWord\hey-jarvis_windows.ppn', 'WakeWord\hi-jarvis_en_windows.ppn']) == True:
+            winsound.Beep(2500, 100)
+        else:
+            WakeWord.reset()
+
+    except:
+        print('Unknown Error')
