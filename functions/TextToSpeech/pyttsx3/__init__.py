7654d852479b47cf0603551ede4b2a61ca7f32ce
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-10-30 02:15:42 +0530

    exe build

--- functions/TextToSpeech/pyttsx3/__init__.py
+++ functions/TextToSpeech/pyttsx3/__init__.py
@@ -2,19 +2,9 @@ import pyttsx3
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-  
-# for voice in voices:
-#     # to get the info. about various voices in our PC 
-#     print("Voice:")
-#     print("ID: %s" %voice.id)
-#     print("Name: %s" %voice.name)
-#     print("Age: %s" %voice.age)
-#     print("Gender: %s" %voice.gender)
-#     print("Languages Known: %s" %voice.languages)
-
-# Changing index, changes voices. o
-engine.setProperty('voice', voices[4].id)
-engine.setProperty('rate', 150)    # Speed percent (can go over 100)
+engine.setProperty('voice', voices[0].id)
+voicespeed=170
+engine.setProperty('rate', voicespeed)
 engine.say("hello, sir i am your voice assistant")
 engine.say("how can i help you")
 engine.runAndWait()
