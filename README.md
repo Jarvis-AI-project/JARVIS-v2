fc88b43fce776f691c71435d888bd98d85dc7447
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-11-06 11:36:48 +0530

    added ReadMe

---
+++ README.md
@@ -0,0 +1,22 @@
+THIS IS A HOBBY PROJECT
+
+# **CREDITS:**
+
+| Module                             | Done By                          |
+| ---------------------------------- | -------------------------------- |
+| Face Recognition                   | Devasheesh x Dhruv Singh Poniya  |
+| Passcode + Encryption (GUI + Code) | Swarnim Burniwal x _____________ |
+| Intent Recognition (azure)         | Devasheesh                       |
+| Spotify Intent Traning             | Krishna Bhardwaj                 |
+| Spotify Driver Code                | Devasheesh                       |
+| Object Detection (YoLo v5)         | Devasheesh x Swarnim             |
+| Todoist                            | --------------                   |
+| SpeechToText (azure)               | Devasheesh                       |
+| TextToSpeech (IVONA)               | Swarnim x Devasheesh             |
+| WakeWord                           | Devasheesh                       |
+| Fingerprint                        | -------------                    |
+| GoogleMeetRecorder                 | -------------                    |
+|                                    |                                  |
+|                                    |                                  |
+|                                    |                                  |
+|                                    |                                  |
--- __main__.py
+++ __main__.py
@@ -1,59 +1,7 @@
-import functions.WakeWord as WakeWord
-import functions.SpeechToText.azure as STT
-import winsound
-import features.DateTime.time as time
-import re
-import features.music.spotify as spotify
-import features.music.spotify.azure_sdk as azure_sdk
-
-# verification
-import functions.FaceRecognition as FaceID
-if FaceID.FaceID() == True:
-    print("FaceID: Verified")
-elif FaceID.FaceID() == False:
-    print("FaceID: Not Verified")
-    us = input('Do you want to verify by password? (y/n): ')
-    if us.lower() == 'y':
-        passw = input('Password: ')
-        if passw == '1234':
-            print('Password: Verified')
-        else:
-            print('Password: Not Verified')
-            exit()
-    else:
-        print('Password: Not Verified')
-        exit()
+#contains gui code of login screen 
 
 
-while True:
-    wake = WakeWord.wake_word_detection(
-        model=['functions\WakeWord\jarvis_windows.ppn', 'functions\WakeWord\hey-jarvis_windows.ppn', 'functions\WakeWord\hi-jarvis_en_windows.ppn'])
-    try:
-        if wake == True:
-            winsound.Beep(2500, 100)
-            output_dict = STT.speech_to_text()
-            print(output_dict)
-            command = (output_dict['transcription']).lower()
-            if command != '':
-                print('Analysing...')
-                user_intent = azure_sdk.azure_query(command, "JARVIS-MAIN", 'v0.1-dep1')
-                # --------<features here>--------
-                if user_intent['intent'] == 'Jarvis.MusicControls':
-                    spotify.spotify_sdk(command)
-                
-                #elif
 
-                else:
-                    print('Command not found')
 
-                # --------</features here>--------
-
-        elif wake == False:
-            WakeWord.reset()
-
-        elif wake == 'KeyboardInterrupt':
-            break
-
-    except Exception as e:
-        print('Unknown Error')
-        print(e)
+#do not encrypt files that are not requred to run jarvis at this moment
+#if verification is successfull decrypt all "requred" files and redirect to main_jarvis.py
